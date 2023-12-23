bdde403a79fc2cb17ed1d75de1c0f16c57f5e107
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-12-23 09:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstChar(text) {
   // your code here
+	if(text.split(' ').every((e)=>e.length===0))
+		return ''
+	const a =text.split(' ');
+	if(a[0].length===0)
+		return a[1].charAt(0);
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
